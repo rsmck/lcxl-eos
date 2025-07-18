91232f9cfac261ae5b038046c172b676a22338f8
Ross Henderson-McKillop <ross@rsmck.co.uk>
2025-07-18 23:39:07 +0100

    Improved fader handling

--- lcxl_osc.js
+++ lcxl_osc.js
@@ -25,19 +25,34 @@ const util = require ("util");
 const EOS_CONSOLE_IP = "127.0.0.1";
 const EOS_PROTO = 'tcp';
 const EOS_CONSOLE_PORT = 5604;
+const FADER_MISMATCH_CATCH = true;
 const DEBUG = false;
 
 // State
 var strLastCueState = '';
-var intLastAct = 0;
 var bolShiftPressed = false;
 var bolReady = false;
 var bolDeskLocked = false;
-
+var intLastAct = 0;
+var timFaderUpdate = false;
+var bolFadeFlash = 0;
 
 // Definitions 
 var wheelNames = new Array(128);
 var controlColours = new Array(128);
+var faderNames = new Array(9);
+var faderLevels = new Array(9);
+var faderLevelsLocal = new Array(9);
+
+// Fader Names
+faderNames[1] = 'Fader 1';
+faderNames[2] = 'Fader 2';
+faderNames[3] = 'Fader 3';
+faderNames[4] = 'Fader 4';
+faderNames[5] = 'Fader 5';
+faderNames[6] = 'Fader 6';
+faderNames[7] = 'Fader 7';
+faderNames[8] = 'Fader 8';
 
 // What do the encoders do
 wheelNames[13] = 'frame_angle_a';
@@ -95,25 +110,7 @@ controlColours[34] = 37; // cyan
 controlColours[35] = 53; // magents
 controlColours[36] = 13; // yellow
 
-// fader buttons
-controlColours[37] = 9;
-controlColours[38] = 9;
-controlColours[39] = 9;
-controlColours[40] = 9;
-controlColours[41] = 9;
-controlColours[42] = 9;
-controlColours[43] = 9;
-controlColours[44] = 9;
-
-controlColours[45] = 22;
-controlColours[46] = 22;
-controlColours[47] = 22;
-controlColours[48] = 22;
-controlColours[49] = 22;
-controlColours[50] = 22;
-controlColours[51] = 22;
-controlColours[52] = 22;
-
+// last next
 controlColours[103] = 9;
 controlColours[102] = 9;
 
@@ -186,6 +183,8 @@ osc.send({ address: '/eos/fader/1/config/8' }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT)
 osc.send({ address: '/eos/subscribe', args: [{type: "f", value: 1}] }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
 osc.send({ address: '/eos/subscribe/wheel', args: [{type: "f", value: 1}] }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
 osc.send({ address: '/eos/subscribe/param', args: [{type: "f", value: 1}] }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
+osc.send({ address: '/eos/subscribe/fader', args: [{type: "f", value: 1}] }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
+osc.send({ address: '/eos/subscribe/out/fader', args: [{type: "f", value: 1}] }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
 
 // Configure the LCXL
 output.sendMessage([240,0,32,41,2,21,2,127,247]);
@@ -205,11 +204,13 @@ function encoderPop(a,b,c) {
   output.sendMessage([240, 0, 32, 41, 2, 21, 4, 54, 0x01, 247])
   output.sendMessage([ 240, 0, 32, 41, 2, 21, 6, 54, 0, ...a.split('').map(c=>c.charCodeAt(0)), 247] );
   output.sendMessage([ 240, 0, 32, 41, 2, 21, 6, 54, 1, ...b.split('').map(c=>c.charCodeAt(0)), 247] );
-//  output.sendMessage([ 240, 0, 32, 41, 2, 21, 6, 54, 2, ...c.split('').map(c=>c.charCodeAt(0)), 247] );
   output.sendMessage([ 240, 0, 32, 41, 2, 21, 4, 54, 0x7F, 247 ]);
 }
 
 function displayMain(a,b,c) {
+  if (typeof a == 'undefined') a = '';
+  if (typeof b == 'undefined') b = '';
+  if (typeof c == 'undefined') c = '';
   output.sendMessage([240, 0, 32, 41, 2, 21, 4, 53, 0x02, 247])
   output.sendMessage([ 240, 0, 32, 41, 2, 21, 6, 53, 0, ...a.split('').map(c=>c.charCodeAt(0)), 247] );
   output.sendMessage([ 240, 0, 32, 41, 2, 21, 6, 53, 1, ...b.split('').map(c=>c.charCodeAt(0)), 247] );
@@ -224,11 +225,41 @@ for (let control = 1; control < 164; control++) {
   }
 }
 
+function updateFaders() {
+  for (let i = 1; i < faderNames.length; i++) {
+    topBtn = i+36;
+    btmBtn = i+44;
+    if (faderNames[i] != '') {
+      // this fader has something on it
+      if (faderNames[i].toLowerCase().includes('inhib')) {
+        output.sendMessage([176,btmBtn,5]);
+      } else {
+        output.sendMessage([176,btmBtn,22]);
+      }
+      output.sendMessage([176,topBtn,9]);
+      if ((faderLevels[i] != faderLevelsLocal[i])) {
+        if (bolFadeFlash == 1) {
+          output.sendMessage([176,btmBtn,0]);
+        }
+      }
+    } else {
+      output.sendMessage([176,topBtn,0]);
+      output.sendMessage([176,btmBtn,0]);
+    }
+  }
+  if (bolFadeFlash == 1) {
+    bolFadeFlash = 0;
+  } else {
+    bolFadeFlash = 1;
+  }
+}
+
 // Main Handler
 input.on('message', (deltaTime, message) => {
   const z = message[0];
   const ch = message[1];
   const val = message[2];
+  const now = Date.now();
 
   // Faders
   if (z == 190) {
@@ -246,7 +277,7 @@ input.on('message', (deltaTime, message) => {
   } else if (ch == 104 && val == 127) {
     // weird hidden button
     osc.send({
-        address: '/eos/key/select_last',
+        address: '/eos/key/last',
     }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
   } else if (ch == 103 && val == 127) {
     // prev
@@ -269,8 +300,30 @@ input.on('message', (deltaTime, message) => {
         address: '/eos/key/stop',
     }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
   } else if (z == 191 && (ch >= 5 && ch <= 12)) {
-    const strFaderMsg = '/eos/fader/1/'+(ch-4);
-    encoderPop('Fader '+(ch-4),(val/127*100).toFixed(2)+'%',' ');
+    const faderId = ch-4;
+    if (faderNames[faderId] == '') return;
+
+    const strFaderMsg = '/eos/fader/1/'+faderId;
+    faderLevelsLocal[faderId] = (val/127).toFixed(2);
+
+    if (FADER_MISMATCH_CATCH && now > intLastAct+400 && !bolShiftPressed) {
+      if (Math.abs(faderLevelsLocal[faderId]-faderLevels[faderId]) > 0.03) {
+        if (faderLevelsLocal[faderId] > faderLevels[faderId]) {
+          encoderPop(faderNames[faderId], 'vv ( '+(faderLevels[faderId]*100).toFixed(2)+'% ) vv');
+        } else {
+          encoderPop(faderNames[faderId], '^^ ( '+(faderLevels[faderId]*100).toFixed(2)+'% ) ^^');
+        }
+        return;
+      }
+    }
+
+    encoderPop(faderNames[faderId], (val/127*100).toFixed(2)+'%');
+
+    faderLevelsLocal[faderId] = (val/127).toFixed(2);
+    faderLevels[(ch-4)] = (val/127).toFixed(2);
+
+    intLastAct = Date.now();
+
     osc.send({
         address: strFaderMsg,
         args: [{type: "f", value: val/127}]
@@ -314,7 +367,6 @@ input.on('message', (deltaTime, message) => {
           args: [{type: "f", value: dir}]
       }, EOS_CONSOLE_IP, EOS_CONSOLE_PORT);
     }
-    intLastAct = Date.now();
   } else {
     if (DEBUG) console.log(`Received UNKNOWN MIDI message: [${message.join(', ')}] (deltaTime: ${deltaTime})`);
   }
@@ -331,11 +383,25 @@ osc.on("message", function (oscMsg) {
     }
     strLastCueState = oscMsg.args[0].value;
   } else if (bolReady && oscMsg.address.match(/\/active\/wheel/i)) {
+    var now = Date.now();
     if (now > intLastAct+300) return;
     const parts = oscMsg.args[0].value.match(/(.*)\[/);
     const label = parts[1].trim();
     const value = oscMsg.args[2].value.toFixed(2)+'';
     encoderPop(label,value,'');
+  } else if (bolReady && oscMsg.address.match(/\/eos\/fader\/1/i)) {
+    var now = Date.now();
+    // if (now > intLastAct+900) return;
+    const parts = oscMsg.address.match(/\/eos\/fader\/1\/([0-9])/);
+    const faderId = parts[1];
+    const value = oscMsg.args[0].value.toFixed(2);
+    faderLevels[faderId] = value;
+  } else if (oscMsg.address.match(/\/eos\/out\/fader\/1\/([0-9])\/name/i)) {
+    console.log('fader name');
+    const parts = oscMsg.address.match(/\/eos\/out\/fader\/1\/([0-9])/);
+    const faderId = parts[1];
+    const value = oscMsg.args[0].value;
+    faderNames[faderId] = value;
   } else if (oscMsg.address == '/eos/out/event/locked') {
     const parts = strLastCueState.split(' '); 
     if (oscMsg.args[0].value == 1) {
@@ -349,5 +415,5 @@ osc.on("message", function (oscMsg) {
 });
 
 setTimeout(function() { bolReady = true; }, 2000);
-
+setInterval(updateFaders, 400);
 console.log('Running');
